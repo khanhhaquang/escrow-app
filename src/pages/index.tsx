7bf3c1f68fdf8d261e2120411b191006c20572ab
Khanh Ha <khanhhaquang@gmail.com>
2023-04-10 15:14:32 +0700

    update: add cannon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,32 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Stars } from '@react-three/drei';
+import { OrbitControls, Sky } from '@react-three/drei';
+import { Physics, Triplet, useBox, usePlane } from '@react-three/cannon';
+import { BufferGeometry, Material, Mesh } from 'three';
 
-const Box = () => {
+const Box = (props: any) => {
+	const [ref, api] = useBox<Mesh<BufferGeometry, Material>>(() => ({ mass: 0.5, position: [0, 5, 0], ...props }));
+
 	return (
-		<mesh>
-			<sphereGeometry attach='geometry' />
+		<mesh
+			onClick={() => {
+				api.velocity.set(0, 10, 0);
+			}}
+			position={[0, 5, 0]}
+			ref={ref}
+		>
+			<sphereBufferGeometry attach='geometry' />
 			<meshStandardMaterial color='sandybrown' attach='material' />
+		</mesh>
+	);
+};
+
+const Plane = (props: any) => {
+	const rotation: Triplet = [-Math.PI / 2, 0, 0];
+	const [ref] = usePlane<Mesh<BufferGeometry, Material>>(() => ({ rotation, ...props }));
+	return (
+		<mesh position={[0, 0, 0]} ref={ref} rotation={rotation}>
+			<planeBufferGeometry attach='geometry' args={[100, 100]} />
+			<meshLambertMaterial color='azure' attach='material' />
 		</mesh>
 	);
 };
@@ -16,10 +37,13 @@ export default function Home() {
 			<Canvas className='w-full h-full'>
 				<color attach='background' args={['#000']} />
 				<OrbitControls />
-				<Stars />
+				<Sky />
 				<ambientLight intensity={0.8} />
-				<spotLight position={[10, 10, 5]} angle={0.2} />
-				<Box />
+				<spotLight position={[10, 20, 2]} angle={0.2} />
+				<Physics>
+					<Plane />
+					<Box />
+				</Physics>
 			</Canvas>
 		</main>
 	);
